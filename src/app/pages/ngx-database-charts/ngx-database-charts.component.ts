a435a9b5f7a7df09dbf58b6a4b66d6d790cc5aa1
Chen Wei <Chen.Wei@sony.com>
2021-08-18 09:52:24 +0800

    charts报表新增

---
+++ src/app/pages/ngx-database-charts/ngx-database-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-database-charts',
+  templateUrl: './ngx-database-charts.component.html',
+  styleUrls: ['./ngx-database-charts.component.scss']
+})
+export class NgxDatabaseChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { NgxDatabaseComponent } from './ngx-database/ngx-database.component';
+import { NgxDatabaseChartsComponent } from './ngx-database-charts/ngx-database-charts.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/ngx-database' },
@@ -10,11 +11,15 @@ const routes: Routes = [
     path: 'ngx-database',
     component: NgxDatabaseComponent,
   },
+  {
+    path: 'ngx-database-charts',
+    component: NgxDatabaseChartsComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [NgxDatabaseComponent],
+  declarations: [NgxDatabaseComponent, NgxDatabaseChartsComponent],
   imports: [RouterModule.forRoot(routes), CommonModule, NgxDatatableModule],
   exports: [RouterModule],
 })
-export class PagesModule { }
+export class PagesModule {}
