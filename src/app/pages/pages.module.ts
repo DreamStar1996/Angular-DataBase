6d857348bf645718f1dfb0d391321610c6579228
Chen Wei <Chen.Wei@sony.com>
2021-08-23 13:53:33 +0800

    新增ngxdatatable报表

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
-const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-];
+const routes: Routes = [{ path: 'welcome', component: WelcomeComponent }];
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [RouterModule.forRoot(routes), CommonModule, NgxDatatableModule],
   exports: [RouterModule],
 })
 export class PagesModule {}
