8cb79a23c5e5cd30d744d0d64cf64c037261f8c2
Chen Wei <Chen.Wei@sony.com>
2021-08-23 16:03:44 +0800

    清除报错代码

--- src/app/pages/table-database/ngx-database/ngx-database.component.ts
+++ src/app/pages/table-database/ngx-database/ngx-database.component.ts
@@ -1,32 +1,13 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild } from "@angular/core";
-import { ColumnMode, SortType } from "@swimlane/ngx-datatable";
+import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-ngx-database",
   templateUrl: "./ngx-database.component.html",
   styleUrls: ["./ngx-database.component.scss"],
 })
 export class NgxDatabaseComponent implements OnInit {
-  rows = [];
-
-  ColumnMode = ColumnMode;
 
   constructor() {}
 
   ngOnInit() {
-    this.fetch((data) => {
-      this.rows = data;
-    });
-  }
-
-  fetch(cb) {
-    const req = new XMLHttpRequest();
-    req.open("GET", `assets/data/company.json`);
-
-    req.onload = () => {
-      const data = JSON.parse(req.response);
-      cb(data);
-    };
-
-    req.send();
   }
 }
