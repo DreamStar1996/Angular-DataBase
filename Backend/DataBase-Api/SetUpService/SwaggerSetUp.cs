c84e3c3821dfbc444182052b4372203e9878e8aa
Wei, Chen <Chen.Wei@sony.com>
2021-08-30 18:13:40 +0800

    初创DatabaseAPI

---
+++ Backend/DataBase-Api/SetUpService/SwaggerSetUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace DataBase_Api.SetUpService
+{
+    public static class SwaggerSetUp
+    {
+        public static void AddSwaggerSetup(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            var ApiName = "Webapi.Core";
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    // {ApiName} 定义成全局变量，方便修改
+                    Version = "V1",
+                    Title = $"{ApiName} 接口文档——Angular-Database Api",
+                    Description = $"{ApiName} HTTP API V1",
+
+                });
+                c.OrderActionsBy(o => o.RelativePath);
+            });
+
+        }
+    }
+}
