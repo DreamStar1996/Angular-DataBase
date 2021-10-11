e5605295268325372fff395e8c5d516e7a4638f6
Wei, Chen <Chen.Wei@sony.com>
2021-10-11 13:22:28 +0800

    Update ng-Charts

--- Frontend/src/app/pages/table-base-charts/table-base-charts.module.ts
+++ Frontend/src/app/pages/table-base-charts/table-base-charts.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 
+import { MatGridListModule } from "@angular/material/grid-list";
+
 import { NgxChartsComponent } from "./ngx-charts/ngx-charts.component";
 import { NgChartsComponent } from "./ng-charts/ng-charts.component";
 import { ZorroChartsComponent } from "./zorro-charts/zorro-charts.component";
@@ -23,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [NgxChartsComponent, NgChartsComponent, ZorroChartsComponent],
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [RouterModule.forRoot(routes), CommonModule, MatGridListModule],
   exports: [RouterModule],
 })
 export class TableBaseChartsModule {}
