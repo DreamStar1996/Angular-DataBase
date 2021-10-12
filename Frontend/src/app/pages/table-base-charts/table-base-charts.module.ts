393de97aabde9512c8842d3190d7ffd7daaca470
Wei, Chen <Chen.Wei@sony.com>
2021-10-12 12:59:39 +0800

    增加模板几个类型

--- Frontend/src/app/pages/table-base-charts/table-base-charts.module.ts
+++ Frontend/src/app/pages/table-base-charts/table-base-charts.module.ts
@@ -7,6 +7,12 @@ import { MatGridListModule } from "@angular/material/grid-list";
 import { NgxChartsComponent } from "./ngx-charts/ngx-charts.component";
 import { NgChartsComponent } from "./ng-charts/ng-charts.component";
 import { ZorroChartsComponent } from "./zorro-charts/zorro-charts.component";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 const routes: Routes = [
   {
@@ -25,7 +31,18 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [NgxChartsComponent, NgChartsComponent, ZorroChartsComponent],
-  imports: [RouterModule.forRoot(routes), CommonModule, MatGridListModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule,
+    MatGridListModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzGridModule,
+    NzIconModule
+  ],
   exports: [RouterModule],
 })
 export class TableBaseChartsModule {}
